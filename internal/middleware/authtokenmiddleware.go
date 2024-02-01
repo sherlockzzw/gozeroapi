8512e1e1c8cdc0a4e2fab2d340a2faddc249af54
yuqi <43877572+yuqir@users.noreply.github.com>
2024-02-01 13:59:48 +0800

    Update authtokenmiddleware.go

--- internal/middleware/authtokenmiddleware.go
+++ internal/middleware/authtokenmiddleware.go
@@ -21,24 +21,21 @@ func NewAuthTokenMiddleware(c config.Config, redisClient *redis.Client) *AuthTok
 
 func (m *AuthTokenMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		headers, code, err := result.New(m.RedisClient, &w, r, m.Config.Auth.AccessExpire)
+		headers, code, err := result.New(&w, r, m.Config.Auth.AccessExpire)
 		if err != nil {
 			result.ResultFail(r.Context(), w, code, err)
 			return
 		}
-		checkCode, err := headers.Check(r.Header.Get("source"), r.Header.Get("business"))
+		checkCode, err := headers.Check()
 		if err != nil {
 			result.ResultFail(r.Context(), w, checkCode, err)
 			return
 		}
 		next(w, r)
-		token, checkAtCode, err := headers.CheckAccessExpireTime(m.Config.Auth.AccessExpire)
+		checkAtCode, err := headers.CheckAccessExpireTime(m.Config.Auth.AccessExpire)
 		if err != nil {
 			result.ResultFail(r.Context(), w, checkAtCode, err)
 			return
-		}
-		if token != "" {
-			w.Header().Add("renewToken", token)
 		}
 	}
 }
