ac963dbd1b2f53977b6dd82bf8961919cb686376
yuqi <43877572+yuqir@users.noreply.github.com>
2024-02-02 14:47:41 +0800

    test

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,7 +3,14 @@ package config
 import (
 	"context"
 	"encoding/json"
+	"github.com/go-redis/redis/v8"
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/core/service"
 	"github.com/zeromicro/go-zero/rest"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 type Config struct {
@@ -37,4 +44,55 @@ func getRedis(config Config) (redisX *RedisX) {
 	}
 
 	return
+}
+
+func Init() (config *Config) {
+	config = new(Config)
+	configOnce.Do(func() {
+		var (
+			filePath string
+			err      error
+		)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		for {
+			select {
+			case <-ctx.Done():
+				panic("read config yaml file failed")
+			case <-time.After(time.Nanosecond):
+				env := os.Getenv("environ")
+				switch env {
+				case service.ProMode:
+					filePath, err = filepath.Abs("etc/release.apiclipfilm.yaml")
+					if err != nil {
+						panic(err)
+					}
+				case service.TestMode:
+					filePath, err = filepath.Abs("etc/test.apiclipfilm.yaml")
+					if err != nil {
+						panic(err)
+					}
+				default:
+					filePath, err = filepath.Abs("etc/dev.apiclipfilm.yaml")
+					if err != nil {
+						panic(err)
+					}
+				}
+
+				conf.MustLoad(filePath, config)
+
+				logx.WithContext(ctx).Infof("successfully set config, path: %s, env: %s", filePath, env)
+				RegisterEtcd(ctx, config)
+				return
+			}
+		}
+	})
+
+	return
+}
+
+func RegisterRedisCli(config Config) *redis.Client {
+	return initRedis(getRedis(config))
 }
--- internal/config/etcd.go
+++ internal/config/etcd.go
@@ -4,12 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/go-redis/redis/v8"
-	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/core/logx"
-	"github.com/zeromicro/go-zero/core/service"
 	clientv3 "go.etcd.io/etcd/client/v3"
-	"os"
-	"path/filepath"
 	"sync"
 	"time"
 )
@@ -26,54 +22,6 @@ func InItWatchEtcd(config Config) {
 	wg := new(sync.WaitGroup)
 	RegisterEtcd(context.Background(), &config)
 	watchRedis(config, wg)
-}
-
-func Init() (config *Config) {
-	config = new(Config)
-
-	configOnce.Do(func() {
-		var (
-			filePath string
-			err      error
-		)
-
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-
-		for {
-			select {
-			case <-ctx.Done():
-				panic("read config yaml file failed")
-			case <-time.After(time.Nanosecond):
-				env := os.Getenv("environ")
-				switch env {
-				case service.ProMode:
-					filePath, err = filepath.Abs("etc/release.apiclipfilm.yaml")
-					if err != nil {
-						panic(err)
-					}
-				case service.TestMode:
-					filePath, err = filepath.Abs("etc/test.apiclipfilm.yaml")
-					if err != nil {
-						panic(err)
-					}
-				default:
-					filePath, err = filepath.Abs("etc/dev.apiclipfilm.yaml")
-					if err != nil {
-						panic(err)
-					}
-				}
-
-				conf.MustLoad(filePath, config)
-
-				logx.WithContext(ctx).Infof("successfully set config, path: %s, env: %s", filePath, env)
-				RegisterEtcd(ctx, config)
-				return
-			}
-		}
-	})
-
-	return
 }
 
 func RegisterEtcd(ctx context.Context, config *Config) {
@@ -127,8 +75,4 @@ func initRedis(redisX *RedisX) *redis.Client {
 	logx.Infof("successfully connect redis client")
 
 	return RedisClient
-}
-
-func RegisterRedisCli(config Config) *redis.Client {
-	return initRedis(getRedis(config))
 }
