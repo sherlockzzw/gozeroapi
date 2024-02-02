3977feda4d060bb715baacc5fe2997f93b531329
yuqi <43877572+yuqir@users.noreply.github.com>
2024-02-02 14:58:00 +0800

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -39,6 +39,7 @@ func getRedis(config Config) (redisX *RedisX) {
 		panic(err)
 	}
 	redisX = &RedisX{}
+	logx.Infof("test:%+v", redisResp)
 	if err = json.Unmarshal(redisResp.Kvs[0].Value, redisX); err != nil {
 		panic(err)
 	}
